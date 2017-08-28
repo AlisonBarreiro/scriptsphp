f628b99b4aca396813e8ae664c85bccebf60f13a
AlisonBarreiro <alison.matias@cc.ci.ufpb.br>
2017-08-28 17:49:30 -0300

    Update test-mysql.php

--- test-mysql.php
+++ test-mysql.php
@@ -4,17 +4,14 @@ $dbname = 'name';
 $dbuser = 'user';
 $dbpass = 'pass';
 $dbhost = 'host';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
-$tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
-  $tblCnt++;
-  #echo $tbl[0]."<br />\n";
+
+$conn = mysql_connect($dbhost,$dbuser,$dbpass); mysql_select_db($dbname) or die( "Could not connect to MySQL databaseL");
+if (!$conn) {
+  echo "Could not connect to MySQL database."; 
+             exit;}
+else {
+  echo "Congratulations!! The connection to the database occurred normally!.";
 }
-if (!$tblCnt) {
-  echo "There are no tables<br />\n";
-} else {
-  echo "There are $tblCnt tables<br />\n";
-}
+mysql_close(); 
+
+?>
