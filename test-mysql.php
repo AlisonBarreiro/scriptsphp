f10371723f4c2a5c9872b3a3c2234fbb99d9d004
AlisonBarreiro <alison.matias@cc.ci.ufpb.br>
2017-08-28 17:50:28 -0300

    Update test-mysql.php

--- test-mysql.php
+++ test-mysql.php
@@ -1,17 +1,22 @@
 <?php
+
 # Fill in the values corresponding to your project
+
 $dbname = 'name';
 $dbuser = 'user';
 $dbpass = 'pass';
 $dbhost = 'host';
 
 $conn = mysql_connect($dbhost,$dbuser,$dbpass); mysql_select_db($dbname) or die( "Could not connect to MySQL databaseL");
+
 if (!$conn) {
   echo "Could not connect to MySQL database."; 
-             exit;}
+             exit;
+} 
 else {
   echo "Congratulations!! The connection to the database occurred normally!.";
 }
+
 mysql_close(); 
 
 ?>
